4f8c7a18ea0f30a2b6aae2478ca416c94e9787f5
Tristan Smith <darcnite3000@gmail.com>
2018-03-15 11:37:21 +1100

    layout test a

--- index.jsx
+++ index.jsx
@@ -1,10 +1,32 @@
 import Vue from 'vue'
+import './app.css'
 new Vue({
   el: '#app',
+  data() {
+    return {
+      pages: ['header', 1, 2, 3, 4]
+    }
+  },
   render() {
     return (
       <div id="app">
-        <h1> Hello World </h1>
+        {this.pages.map((page, i) => (
+          <div key={i} class="page" id={`page-${page}`}>
+            <h1>Page {page}</h1>
+          </div>
+        ))}
+        <div class="viewport">
+          <a href="#" class="findOut">
+            Find Out More
+          </a>
+          <div class="navbar">
+            {this.pages.map((page, i) => (
+              <a href={`#page-${page}`} key={i}>
+                {page}
+              </a>
+            ))}
+          </div>
+        </div>
       </div>
     )
   },
