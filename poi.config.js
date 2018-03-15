b1fc1681bf77e276a5da57916d7ae897feef67c3
Tristan Smith <darcnite3000@gmail.com>
2018-03-15 11:36:30 +1100

    disable prerender for now

--- poi.config.js
+++ poi.config.js
@@ -1,10 +1,9 @@
-const path = require('path')
-module.exports = ({ mode }) => ({
-  entry: './index.jsx',
-  presets: [
-    require('poi-preset-prerender-spa')(
-      {},
-      { renderAfterDocumentEvent: 'render-event' }
-    )
-  ]
-})
+module.exports = {
+  entry: './index.jsx'
+  // presets: [
+  //   require('poi-preset-prerender-spa')(
+  //     {},
+  //     { renderAfterDocumentEvent: 'render-event' }
+  //   )
+  // ]
+}
