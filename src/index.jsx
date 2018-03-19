9046c818901db38dabb9c540c12af35d42018c31
Tristan Smith <darcnite3000@gmail.com>
2018-03-19 12:47:41 +1100

    extract page

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import ScrollTo from './ScrollTo'
 import Particles from './Particles'
 import Mousey from './Mousey'
 import BlendLogo from './BlendLogo'
+import Page from './Page'
 import 'particles.js'
 import './app.css'
 
@@ -12,54 +13,6 @@ smoothscroll.polyfill()
 
 Vue.use(KsVueScrollmagic)
 Vue.prototype.$particles = window.particlesJS
-
-const Page = {
-  props: {
-    label: { type: [String, Number], default: 0 }
-  },
-  data() {
-    const pageId = `page-${this.label}`
-    const navClass = `nav-${pageId}`
-    return {
-      showName: `pageShow${this.label}`,
-      visibleName: `pageVisible${this.label}`,
-      pageId,
-      navClass
-    }
-  },
-  render() {
-    return (
-      <div class="page" id={this.pageId}>
-        {this.$slots.default || <h1>Page {this.label}</h1>}
-      </div>
-    )
-  },
-  mounted() {
-    this.$nextTick(() => {
-      const tweenIn = this.$gsap.TweenMax.staggerFrom(
-        `#${this.pageId} > *`,
-        1,
-        { y: '+=150', autoAlpha: 0 },
-        0.2
-      )
-      const onShow = new this.$scrollmagic.Scene({
-        triggerElement: `#${this.pageId}`
-      }).setTween(tweenIn)
-      this.$ksvuescr.$emit('addScene', this.showName, onShow)
-      if (document.getElementsByClassName(this.navClass).length) {
-        const isVisible = new this.$scrollmagic.Scene({
-          triggerElement: `#${this.pageId}`,
-          duration: '100%'
-        }).setClassToggle(`.${this.navClass}`, 'active')
-        this.$ksvuescr.$emit('addScene', this.visibleName, isVisible)
-      }
-    })
-  },
-  beforeDestroy() {
-    this.$ksvuescr.$emit('destroyScene', this.visibleName)
-    this.$ksvuescr.$emit('destroyScene', this.showName)
-  }
-}
 
 new Vue({
   el: '#app',
