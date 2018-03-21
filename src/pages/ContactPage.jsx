7257513e08f8d686a957a818251fb5d3a5d83c8c
Tristan Smith <darcnite3000@gmail.com>
2018-03-21 14:49:51 +1100

    add submission notice

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -3,13 +3,78 @@ export default {
   name: 'ContactPage',
   data() {
     return {
+      sent: false,
+      error: '',
       purpose: '',
       name: '',
       email: '',
       message: ''
     }
   },
+  methods: {
+    submitContact() {
+      const data = {
+        purpose: this.purpose,
+        name: this.name,
+        email: this.email,
+        message: this.message
+      }
+      console.log(data)
+    }
+  },
   render() {
+    const contact = this.sent ? (
+      <div class="contact-form sent">
+        Thank you for your enquiry.<br />A member of our team will be in touch
+        with you shortly.
+      </div>
+    ) : (
+      <form
+        class="contact-form"
+        action="#"
+        onSubmit:prevent={this.submitContact}
+      >
+        <div class="details">
+          <div class="form-group">
+            <label>purpose of enquiry</label>
+            <select v-model={this.purpose}>
+              <option>new broker enquiries</option>
+              <option>existing broker submissions</option>
+              <option>claims enquiries</option>
+              <option>other enquiries</option>
+            </select>
+          </div>
+          <div class="form-group">
+            <label>your name</label>
+            <input
+              type="text"
+              placeholder="leonardo splinterson"
+              v-model={this.name}
+            />
+          </div>
+          <div class="form-group">
+            <label>your email address</label>
+            <input
+              type="email"
+              placeholder="leonardo@tmntinsurance.com.au"
+              v-model={this.email}
+            />
+          </div>
+        </div>
+        <div class="message">
+          <div class="form-group">
+            <label>your message</label>
+            <textarea
+              placeholder="write your message here"
+              v-model={this.message}
+            />
+          </div>
+        </div>
+        <div class="button-group">
+          <input type="submit" value="submit" class="button" />
+        </div>
+      </form>
+    )
     return (
       <Page label="contact">
         <h1>we could be the perfect blend</h1>
@@ -19,47 +84,9 @@ export default {
           altogether, we’d love to hear from you. We’re responsive, responsible
           and reliable, so sing out, and we’ll sing right back.
         </p>
-        <form class="contact-form" action="#">
-          <div class="details">
-            <div class="form-group">
-              <label>purpose of enquiry</label>
-              <select v-model={this.purpose}>
-                <option>new broker enquiries</option>
-                <option>existing broker submissions</option>
-                <option>claims enquiries</option>
-                <option>other enquiries</option>
-              </select>
-            </div>
-            <div class="form-group">
-              <label>your name</label>
-              <input
-                type="text"
-                placeholder="leonardo splinterson"
-                v-model={this.name}
-              />
-            </div>
-            <div class="form-group">
-              <label>your email address</label>
-              <input
-                type="email"
-                placeholder="leonardo@tmntinsurance.com.au"
-                v-model={this.email}
-              />
-            </div>
-          </div>
-          <div class="message">
-            <div class="form-group">
-              <label>your message</label>
-              <textarea
-                placeholder="write your message here"
-                v-model={this.message}
-              />
-            </div>
-          </div>
-          <div class="button-group">
-            <input type="submit" value="submit" class="button" />
-          </div>
-        </form>
+        <transition name="fade" mode="out-in">
+          {contact}
+        </transition>
       </Page>
     )
   }
