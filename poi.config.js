fa8333805473238439c6b967fb27f8ce057ff21a
Tristan Smith <darcnite3000@gmail.com>
2018-03-21 12:11:25 +1100

    remove uneeded for now

--- poi.config.js
+++ poi.config.js
@@ -1,10 +1,4 @@
 module.exports = {
   entry: './src/index.jsx',
   homepage: '/blendinsurancesolutions/'
-  // presets: [
-  //   require('poi-preset-prerender-spa')(
-  //     {},
-  //     { renderAfterDocumentEvent: 'render-event' }
-  //   )
-  // ]
 }
