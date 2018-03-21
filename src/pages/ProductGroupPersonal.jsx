b83d2dc22f576862621efb24c58eabc94ace04b9
Tristan Smith <darcnite3000@gmail.com>
2018-03-21 11:41:21 +1100

    content added

--- src/pages/ProductGroupPersonal.jsx
+++ src/pages/ProductGroupPersonal.jsx
@@ -5,6 +5,38 @@ export default {
       <div class="pop-page">
         <IconGroupPersonal invert />
         <h1>group personal accident</h1>
+        <p>
+          Group Personal Accident and Sickness insurance covers a group of
+          people bound by a common interest such as an employer, association or
+          industry.
+        </p>
+        <p>
+          blend’s personal accident and sickness policy offers a range of
+          benefits that can be tailored to meet the needs of any group,
+          providing insured people with financial support when it’s needed most.
+        </p>
+        <p>
+          In the event of injury or sickness, benefits are payable in the form
+          of lump sum or weekly benefits. The policy also allows for a number of
+          corporate protection benefits in instances where the client has been
+          impacted by an injury or sickness suffered by an employee.
+        </p>
+        <div class="list-head">key features</div>
+        <ul>
+          <li>Accidental death</li>
+          <li>Weekly benefits for sickness and injury</li>
+          <li>Sickness and injury resulting in surgery</li>
+          <li>Injury resulting in fractured bones</li>
+          <li>Injury resulting in loss or damage to teeth</li>
+          <li>
+            Personal wellbeing - 17 benefit categories including accidental HIV
+            infection, rehabilitation and financial advice benefits.
+          </li>
+          <li>
+            Corporate protection including corporate image, repatriation and
+            staff recruitment benefits.
+          </li>
+        </ul>
       </div>
     )
   }
