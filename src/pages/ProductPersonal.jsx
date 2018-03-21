e7345feba5e505e9bbeeadf8b9361569404634c5
Tristan Smith <darcnite3000@gmail.com>
2018-03-21 11:57:32 +1100

    add content

--- src/pages/ProductPersonal.jsx
+++ src/pages/ProductPersonal.jsx
@@ -5,6 +5,72 @@ export default {
       <div class="pop-page">
         <IconPersonal invert />
         <h1>personal accident</h1>
+        <p>
+          Our Individual Personal Accident product covers a variety of
+          occupations. The product provides lump sum benefits due to death or
+          permanent disability from accidental injury and weekly benefits for
+          temporary disablement due to accidental, injury or sickness.
+        </p>
+        <p>
+          Even if you already hold some type of insurance, your existing
+          policies may not cover you for serious accidents that can prevent you
+          from continuing your busy lifestyle or returning to work. A serious
+          injury can have a huge impact on you and your family due to medical
+          expenses, reduced income, and your inability to return to normal
+          duties at work.
+        </p>
+        <p>
+          Blend Personal Accident Insurance provides a lump sum payout on
+          eligible claims regardless of whether you return to work or not. This
+          cover provides certainty in what is often a very uncertain and
+          unsettling time.
+        </p>
+        <div class="list-head">key features</div>
+        <ul>
+          <li>No medical forms</li>
+          <li>Flexible premiums and adjustable cover</li>
+          <li>
+            Lump sum cash payment under the Serious Accident Benefit depending
+            on the level of cover chosen
+          </li>
+          <li>
+            Lump sum cash payment on all eligible claims. If it’s an eligible
+            serious accident claim, we’ll pay even if you choose not to return
+            to work
+          </li>
+          <li>Use the cash payments however you like – no restrictions</li>
+          <li>Pays regardless of other insurance products you may have</li>
+          <li>
+            Free 24/7 referral helpline for emergency legal and medical
+            assistance, home support services and home modification assistance
+            in Australia
+          </li>
+          <li>Global cover 24/7, even if you’re overseas on holiday</li>
+          <li>
+            Emergency Assistance - Unlimited access to our emergency assistance
+            team
+          </li>
+          <li>
+            Provide access to medical practitioners for emergency assistance and
+            advice
+          </li>
+          <li>
+            Arrange emergency medical evacuations as a direct result of an
+            injury or sickness
+          </li>
+          <li>
+            Arrange for an accompanying person to travel with and/or remain with
+            an insured person suffering from an injury or sickness
+          </li>
+          <li>
+            Provide payment guarantees and insurance verification to hospitals
+          </li>
+          <li>
+            Repatriation to the insured person's country of residence or a more
+            suitable medical facility following an injury or sickness
+          </li>
+          <li>On-going medical monitoring</li>
+        </ul>
       </div>
     )
   }
