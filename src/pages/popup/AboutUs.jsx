94ab5a1c410ae25f38e0c55e0116de5f9fd25775
Tristan Smith <darcnite3000@gmail.com>
2018-03-28 13:33:10 +1100

    extract about us

--- src/pages/popup/AboutUs.jsx
+++ src/pages/popup/AboutUs.jsx
@@ -1,46 +1,23 @@
 import BlendLogo from '../../BlendLogo'
 import FooterBlurb from '../../FooterBlurb'
+import marked from 'marked'
+import axios from 'axios'
 
 export default {
   name: 'AboutUs',
+  data() {
+    return { content: '' }
+  },
+  mounted() {
+    axios.get('AboutUs.md').then(({ data }) => {
+      this.content = marked(data)
+    })
+  },
   render() {
     return (
       <div class="pop-page" id="page-aboutus">
         <BlendLogo class="logo-pop" />
-        <h1>blend a lloyds syndicate service company</h1>
-        <p>
-          Advent, our capacity provider, has over half a century of underwriting
-          and claims experience.
-        </p>
-        <ul>
-          <li>Experts in over 30 different classes of business</li>
-          <li>
-            Focused on new products and lines of business, enhancing existing
-            products, as well as adopting new technological advancements.
-          </li>
-          <li>A ‘one-stop shop’ for speed of decision making.</li>
-          <li>A progressive Lloyd’s syndicate owned by Fairfax Financial.</li>
-          <li>
-            Access to large distribution channels enabling a truly global reach.
-          </li>
-        </ul>
-        <p>
-          Being a Lloyd’s syndicate gives Advent additional levels of security.
-        </p>
-        <ul>
-          <li>
-            Established in 1765, Lloyd’s is a trusted and world-renowned
-            insurance market.
-          </li>
-          <li>
-            All Lloyd’s syndicates are covered with the additional Lloyd’s chain
-            of security.
-          </li>
-          <li>
-            Being a member of the Lloyd’s market gives Advent wider geographical
-            reach through Lloyd’s international licensing.
-          </li>
-        </ul>
+        <div class="md" domPropsInnerHTML={this.content} />
         <div class="logos">
           <img src="Lloyds.png" alt="Lloyds" />
           <img src="Advent.png" alt="Advent - a fairfax company" />
