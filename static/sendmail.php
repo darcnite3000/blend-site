cbb33ae477eae3a55a64c5c4d06a61a947dbb783
Tristan Smith <darcnite3000@gmail.com>
2018-04-06 10:20:27 +1000

    first pass subscribe to mailchimp list

--- static/sendmail.php
+++ static/sendmail.php
@@ -23,3 +23,46 @@ switch ($purpose) {
 }
 $mailBody = $message. "\r\n". "\r\n"."from: $name<$email>"."\r\n"."Marketing: ".($marketing?"yes":"no");
 mail($to,$purpose,$mailBody,$headers);
+
+if($marketing){
+  $data = [
+    'email' => $email,
+    'status' => 'subscribed',
+    'name' => $name
+  ];
+
+  syncMailchimp($data);
+}
+
+function syncMailchimp($data) {
+
+  $listId = 'c3d255b1d6';
+
+  $memberId = md5(strtolower($data['email']));
+  $dataCenter = substr($apiKey,strpos($apiKey,'-')+1);
+  $url = 'https://' . $dataCenter . '.api.mailchimp.com/3.0/lists/' . $listId . '/members/' . $memberId;
+
+  $json = json_encode([
+      'email_address' => $data['email'],
+      'status'        => $data['status'], // "subscribed","unsubscribed","cleaned","pending"
+      'merge_fields'  => [
+          'NAME'     => $data['name']
+      ]
+  ]);
+
+  $ch = curl_init($url);
+
+  curl_setopt($ch, CURLOPT_USERPWD, 'user:' . $apiKey);
+  curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+  curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+
+  $result = curl_exec($ch);
+  $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+  curl_close($ch);
+
+  return $httpCode;
+}
