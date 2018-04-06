a05721a74ad1dd839c8e0fd90784254a9fa5df33
Tristan Smith <darcnite3000@gmail.com>
2018-04-06 10:30:49 +1000

    extract config into separate file so as to not be public

--- static/sendmail.php
+++ static/sendmail.php
@@ -1,4 +1,5 @@
 <?php
+include 'config.php';
 $post = json_decode(file_get_contents('php://input'),true);
 $purpose=$post['purpose'];
 $name=$post['name'];
@@ -6,21 +7,12 @@ $email=$post['email'];
 $message=$post['message'];
 $marketing=$post['marketing'] == true || $post['marketing'] == "true";
 $to = '';
-switch ($purpose) {
-  case 'new broker enquiries':
-    $to = 'broker@blendinsurance.com.au';
-    break;
-  case 'existing broker submissions':
-    $to = "submissions@blendinsurance.com.au";
-    break;
-  case 'claims enquiries':
-    $to = 'claims@fullertonhealthcs.com.au';
-    break;
-  default:
-    $purpose = 'other enquiries';
-    $to = 'chris@blendinsurance.com.au';
-    break;
+global $purposes;
+global $defaultPurpose;
+if(!isset($purposes[$purpose])){
+  $purpose = $defaultPurpose;
 }
+$to = $purposes[$purpose];
 $mailBody = $message. "\r\n". "\r\n"."from: $name<$email>"."\r\n"."Marketing: ".($marketing?"yes":"no");
 mail($to,$purpose,$mailBody,$headers);
 
@@ -35,8 +27,8 @@ if($marketing){
 }
 
 function syncMailchimp($data) {
-  $listId = 'c3d255b1d6';
-
+  global $apiKey;
+  global $listId;
   $memberId = md5(strtolower($data['email']));
   $dataCenter = substr($apiKey,strpos($apiKey,'-')+1);
   $url = 'https://' . $dataCenter . '.api.mailchimp.com/3.0/lists/' . $listId . '/members/' . $memberId;
